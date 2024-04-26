06bca69c35e561816e3836d6af888035e062a348
Sargis <saqobabloyan@gmail.com>
2024-04-26 14:23:01 +0400

    set path url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    router: {
-        base: "/Green-Farm/"
+    app: {
+        baseURL: "/Green-Farm",
+        buildAssetsDir: "assets",
+
     }
 })
