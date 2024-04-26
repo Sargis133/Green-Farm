9fb5db25414cd6130e58502622a8902a11e300a2
Sargis <saqobabloyan@gmail.com>
2024-04-26 14:29:20 +0400

    set path url

--- components/templates/index/config/advantages.ts
+++ components/templates/index/config/advantages.ts
@@ -7,21 +7,21 @@ export const advantagesData = [
     {
         title: ["Современный", "автопарк"],
         text: "Транспорт оборудован терморегистратором для контроля температуры",
-        image: '~/images/advantages/advantages-delivery.png'
+        image: '../public/advantages/advantages-delivery.png'
     },
     {
         title: ["Консолидационный", "склад"],
         text: "На территории отделения компании, в Московской области и в регионах",
-        image: '@@/images/advantages/advantages-home.png'
+        image: '/public/advantages/advantages-home.png'
     },
     {
         title: ["Полная", "ответственность"],
         text: "Страхование и контроль за состоянием груза на каждом этапе перевозки",
-        image: '~~/images/advantages/advantages-security.png'
+        image: 'public/advantages/advantages-security.png'
     },
     {
         title: ["Уровень", "сервиса"],
         text: "Работаем со всеми категориями клиентов, от частных заказчиков до торговых сетей",
-        image: '@/public/images/advantages/advantages-user.png'
+        image: 'images/advantages/advantages-user.png'
     },
 ];
