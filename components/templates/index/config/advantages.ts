93f919ffc5c75650bfd183c21c1299e09f6f02f6
Sargis <saqobabloyan@gmail.com>
2024-04-26 14:00:30 +0400

    change image url

--- components/templates/index/config/advantages.ts
+++ components/templates/index/config/advantages.ts
@@ -2,26 +2,26 @@ export const advantagesData = [
     {
         title: ["География", "перевозок"],
         text: "Все страны входящие в состав Европейского союза и СНГ",
-        image: '.dist/images/advantages/advantages-location.png'
+        image: '~/images/advantages/advantages-location.png'
     },
     {
         title: ["Современный", "автопарк"],
         text: "Транспорт оборудован терморегистратором для контроля температуры",
-        image: '/Green-Farm/images/advantages/advantages-delivery.png'
+        image: '@@/images/advantages/advantages-delivery.png'
     },
     {
         title: ["Консолидационный", "склад"],
         text: "На территории отделения компании, в Московской области и в регионах",
-        image: '../images/advantages/advantages-home.png'
+        image: 'public/images/advantages/advantages-home.png'
     },
     {
         title: ["Полная", "ответственность"],
         text: "Страхование и контроль за состоянием груза на каждом этапе перевозки",
-        image: 'public/images/advantages/advantages-security.png'
+        image: '/public/images/advantages/advantages-security.png'
     },
     {
         title: ["Уровень", "сервиса"],
         text: "Работаем со всеми категориями клиентов, от частных заказчиков до торговых сетей",
-        image: '~/images/advantages/advantages-user.png'
+        image: '~~/public/images/advantages/advantages-user.png'
     },
 ];
